ca5417382f952c0aec9687e616f79b6603eb11d8
ABHIRAJ GAUTAM <49376719+Abhirajgautam28@users.noreply.github.com>
2024-03-28 21:40:08 +0530

    Update Food Classification.py

--- Food Classification.py
+++ Food Classification.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#import necessary libraries
 import os
 import copy
 import torch
@@ -21,29 +14,11 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from collections import OrderedDict
-
 from tqdm.autonotebook import get_ipython
-
-# In[2]:
-
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device
-
-
-# In[3]:
-
-
 import os
 print(os.getcwd())
-
-
-#  **understanding the dataset**
-
-# The code checks if the "food-101" directory exists in the current working directory. If the directory exists, it prints "Dataset already exists". If not, it downloads the Food-101 dataset from a specified URL, extracts the downloaded archive, and prints status messages throughout the process.
-
-# In[4]:
-
 
 if "food-101" in os.listdir():
     print("Dataset already exists")
@@ -54,56 +29,20 @@ else:
     print("Extracting data..")
     get_ipython().system('tar xzvf food-101.tar.gz > /dev/null 2>&1')
     print("Extraction done!")
-
-
-# In[5]:
-
-
-# The below  code reads a list of food classes from the file "classes.txt" located in the "food-101/meta/" directory.
-# It then selects the first 20 classes from this list and appends an additional class called 'other'. The resulting
-# list, named classes_21, contains 21 classes.
 classes = open("./food-101/meta/classes.txt", 'r').read().splitlines()
 classes_21 = classes[:20] + ['other']
 classes_21, len(classes_21)
-
-
-# **identifying training and testing images**
-
-# In[6]:
-
-
-# The code snippet displays the first five entries from two files, test.txt and train.txt, located in the
-# food-101/meta/ directory. It begins by printing "Testing images," followed by the first five lines from test.txt,
-# then prints "Training images" and shows the first five lines from train.txt. The purpose is to preview the contents
-# of these files, likely listing image data for testing and training purposes.
 get_ipython().system('echo "Testing images"')
 get_ipython().system('head -n 5 ./food-101/meta/test.txt')
 get_ipython().system('echo -e "\\nTraining images"')
 get_ipython().system('head -n 5 ./food-101/meta/train.txt | head -n 5')
 
-
-# **exploratory data analysis (for dummies)**
-
-# In[7]:
-
-
-#The prep_df function processes a file containing relative paths of images. It:
-
-#Reads the file and extracts image paths.
-#Constructs full paths for these images using a base directory.
-#Separates the label and image index from the relative paths.
-#Converts this data into a pandas DataFrame with labels and full image paths.
-#Shuffles the rows of the DataFrame randomly.
-#Returns the shuffled DataFrame.
-
 def prep_df(path: str) -> pd.DataFrame:
     array = open(path, 'r').read().splitlines()
 
-    # Getting the full path for the images
     img_path = "./food-101/images/"
     full_path = [img_path + img + ".jpg" for img in array]
 
-    # Splitting the image index from the label
     imgs = []
     for img in array:
         img = img.split('/')
@@ -111,32 +50,17 @@ def prep_df(path: str) -> pd.DataFrame:
         imgs.append(img)
 
     imgs = np.array(imgs)
-    # Converting the array to a data frame
-    imgs = pd.DataFrame(imgs[:,0], imgs[:,1], columns=['label'])
-    # Adding the full path to the data frame
-    imgs['path'] = full_path
 
-    # Randomly shuffling the order to the data in the dataframe
+    imgs = pd.DataFrame(imgs[:,0], imgs[:,1], columns=['label'])
+    imgs['path'] = full_path
     imgs = shuffle(imgs)
 
     return imgs
-
-
-# In[8]:
-
-
-#It reads data from specified files for training and testing images. The first five entries of the training images DataFrame (train_imgs) are then displayed.
 train_imgs = prep_df('./food-101/meta/train.txt')
 test_imgs = prep_df('./food-101/meta/test.txt')
 
 train_imgs.head(5)
 
-
-# In[9]:
-
-
-#The code visualizes 24 random images from the train_imgs DataFrame. It creates a 3x8 grid and populates it with the images. Each image is accompanied by its label as a title, and the axis markings are removed for a cleaner display. The resulting visualization showcases a subset of the training dataset.
-plt.figure(figsize=(20, 5))
 
 num_rows = 3
 num_cols = 8
@@ -153,13 +77,7 @@ for idx in range(num_rows * num_cols):
     plt.title(label)
     plt.axis("off")
 
-
-# **visualization using barplot**
-
-# In[10]:
-
-
-def barplot_vis(imgs_dataframe):# Use the newly integrated Roboto font family for all text.
+def barplot_vis(imgs_dataframe):
     fig, ax = plt.subplots()
 
     new_labels = [row if row in classes_21 else "other" for row in imgs_dataframe.label]
@@ -170,14 +88,12 @@ def barplot_vis(imgs_dataframe):# Use the newly integrated Roboto font family fo
 
     heights = [grouped_train_imgs.get_group(group).shape[0] for group in classes_21]
 
-    # Save the chart so we can loop through the bars below.
     bars = ax.bar(
         x=classes_21,
         height=heights,
         tick_label=classes_21
     )
 
-    # Axis formatting.
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
@@ -186,8 +102,6 @@ def barplot_vis(imgs_dataframe):# Use the newly integrated Roboto font family fo
     ax.set_axisbelow(True)
     ax.yaxis.grid(True, color='#EEEEEE')
     ax.xaxis.grid(False)
-
-    # Add text annotations to the top of the bars.
     bar_color = bars[0].get_facecolor()
     percentage_heights = np.array(heights) / sum(heights)
     for idx in range(len(bars)):
@@ -200,7 +114,6 @@ def barplot_vis(imgs_dataframe):# Use the newly integrated Roboto font family fo
           weight='bold'
       )
 
-    # Add labels and a title.
     ax.set_xlabel('Food Class', labelpad=15, color='#333333')
     ax.set_ylabel('Number of Images', labelpad=15, color='#333333')
     ax.set_title('Visualizing Class Imbalance', pad=15, color='#333333',
@@ -209,19 +122,8 @@ def barplot_vis(imgs_dataframe):# Use the newly integrated Roboto font family fo
     fig.autofmt_xdate(rotation=45)
     fig.set_size_inches(18.5, 4)
 
-
-# In[11]:
-
-
 barplot_vis(train_imgs)
 
-
-# **preparing the data for transformation**
-
-# In[12]:
-
-
-# Data augmentation for training
 train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.RandomResizedCrop(224),
                                        transforms.RandomHorizontalFlip(),
@@ -229,18 +131,12 @@ train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.ToTensor(),
                                        transforms.Normalize([0.485, 0.456, 0.406],
                                                             [0.229, 0.224, 0.225])])
-# Data augmentation for testing
+
 test_transforms = transforms.Compose([transforms.Resize(255),
                                       transforms.CenterCrop(224),
                                       transforms.ToTensor(),
                                       transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])])
-
-
-# In[13]:
-
-
-#The Label_encoder class provides encoding and decoding functionality for dataset labels. It maps each unique label to a corresponding index and vice versa. 
+                                                           [0.229, 0.224, 0.225])]) 
 class Label_encoder:
     def __init__(self, labels):
         labels = list(set(labels))
@@ -256,11 +152,6 @@ encoder = Label_encoder(classes)
 for i in range(20):
     print(encoder.get_label(i), encoder.get_idx( encoder.get_label(i) ))
 
-
-# In[14]:
-
-
-#The class facilitates efficient access to images and their corresponding labels, making it suitable for training/testing in PyTorch.
 class Food20(Dataset):
     def __init__(self, dataframe, transform=None):
         self.dataframe = dataframe
@@ -281,58 +172,21 @@ class Food20(Dataset):
 
         return image, label
 
-
-# In[15]:
-
-
-#preparing the training and testing datasets with the appropriate image transformations to be used in the subsequent stages of a machine learning or deep learning pipeline, likely in conjunction with PyTorch's data loaders.
 train_dataset = Food20(train_imgs, transform=train_transforms)
 test_dataset = Food20(test_imgs, transform=test_transforms)
 
-
-# In[16]:
-
-
-# code initializes two data loaders using PyTorch's DataLoader utility, which facilitates the efficient batching and loading of dataset entries during training and testing
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
-
-# In[17]:
-
-
-# Testing the retrieval of a single image
 for i in range(10):
     image = train_dataset.__getitem__(i)
     print(encoder.get_label(image[1]), image[0].shape)
-
-
-# **creating the ML model**
-
-# In[18]:
-
-
-#The code loads a DenseNet-201 model with pretrained weights from the ImageNet dataset (with 1K classes). Specifically:
-
-#It fetches the pretrained weights for DenseNet-201 trained on ImageNet.
-#It then initializes the DenseNet-201 model using these weights.
-
 weights = models.DenseNet201_Weights.IMAGENET1K_V1
 model = models.densenet201(weights = weights)
 
-
-# In[19]:
-
-
-# Freeze parameters so we don't backprop through them
 for param in model.parameters():
     param.requires_grad = False
-
-
-# In[20]:
-
-
-#downloading a pre-trained model file (food_classifier.pt) from a specific GitHub URL and saving it to the local directory.
+    
 import requests as reqs
 
 url = "https://github.com/Prakhar998/Food-Classification/raw/master/food_classifier.pt"  
@@ -340,11 +194,6 @@ r = reqs.get(url, allow_redirects=True)
 
 open("./food_classifier.pt", "wb").write(r.content)
 
-
-# In[21]:
-
-
-#below code adapts a DenseNet-201 model for a food classification task with 101 classes, loads a pretrained checkpoint into the model, and then prepares the model for computations on a specified device.
 checkpoint_path = "./food_classifier.pt"
 classifier = nn.Sequential(
     nn.Linear(1920,1024),
@@ -357,117 +206,73 @@ model.load_state_dict(torch.load(checkpoint_path,map_location='cpu'),strict=Fals
 
 model.to(device)
 
-
-# In[22]:
-
-
-#hyper parameters
 num_epochs = 3
 
-
-# In[23]:
-
-
-# loss
 loss_fn = nn.CrossEntropyLoss()
 
-# all parameters are being optimized
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=[0.9, 0.999])
 
 model = model.to(device)
-
-
-# **training and testing the model**
-
-# In[24]:
-
 
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
                optimizer: torch.optim.Optimizer,
                device: torch.device):
-  # Put model in train mode
   model.train()
-
-  # Setup train loss and train accuracy values
   train_loss, train_acc = 0, 0
 
   print("--> Training Progress")
-  # Loop through data loader data batches
+
   for batch, (X, y) in enumerate(tqdm(dataloader)):
-      # Send data to target device
+
       images, labels = X.to(device), y.to(device)
 
-      # 1. Forward pass
       y_pred = model(images)
 
-      # 2. Calculate  and accumulate loss
       loss = loss_fn(y_pred, labels)
       train_loss += loss.item()
 
-      # 3. Optimizer zero grad
       optimizer.zero_grad()
 
-      # 4. Loss backward
       loss.backward()
 
-      # 5. Optimizer step
       optimizer.step()
 
-      # Calculate and accumulate accuracy metric across all batches
       y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
       train_acc += (y_pred_class == labels).sum().item()/len(y_pred)
 
-  # Adjust metrics to get average loss and accuracy per batch
   train_loss = train_loss / len(dataloader)
   train_acc = train_acc / len(dataloader)
   return train_loss, train_acc
-
-
-# In[25]:
-
 
 def test_step(model: torch.nn.Module,
               dataloader: torch.utils.data.DataLoader,
               loss_fn: torch.nn.Module,
               device: torch.device):
-  # Put model in eval mode
   model.eval()
 
-  # Setup test loss and test accuracy values
   test_loss, test_acc = 0, 0
 
-  # Turn on inference context manager
   with torch.inference_mode():
       print("--> Testing Progress")
-      # Loop through DataLoader batches
+
       for batch, (X, y) in enumerate(tqdm(dataloader)):
-          # Send data to target device
+
           images, labels = X.to(device), y.to(device)
 
-          # 1. Forward pass
           test_pred_logits = model(images)
 
-          # 2. Calculate and accumulate loss
           loss = loss_fn(test_pred_logits, labels)
           test_loss += loss.item()
 
-          # Calculate and accumulate accuracy
           test_pred_labels = torch.argmax(torch.softmax(test_pred_logits, dim=1), dim=1)
 
           test_acc += ((test_pred_labels == labels).sum().item()/len(test_pred_labels))
 
-  # Adjust metrics to get average loss and accuracy per batch
   test_loss = test_loss / len(dataloader)
   test_acc = test_acc / len(dataloader)
   return test_loss, test_acc
-
-
-# the code provides the machinery to train a deep learning model, assess its performance on a test dataset, and track and save the best-performing version of the model.
-
-# In[26]:
-
 
 def train(model: torch.nn.Module,
           train_dataloader: torch.utils.data.DataLoader,
@@ -476,7 +281,6 @@ def train(model: torch.nn.Module,
           loss_fn: torch.nn.Module,
           epochs: int,
           device: torch.device):
-  # Create empty results dictionary
   history = {
       "train_loss": [],
       "train_acc": [],
@@ -486,7 +290,6 @@ def train(model: torch.nn.Module,
       "best_model": dict()
   }
 
-  # Loop through training and testing steps for a number of epochs
   for epoch in range(epochs):
       print('Epoch {}/{}'.format(epoch + 1, num_epochs))
 
@@ -500,7 +303,6 @@ def train(model: torch.nn.Module,
           loss_fn=loss_fn,
           device=device)
 
-      # Print out what's happening
       print(
           f"Epoch: {epoch+1} | "
           f"train_loss: {train_loss:.4f} | "
@@ -510,7 +312,6 @@ def train(model: torch.nn.Module,
           f"\n\n=============================\n"
       )
 
-      # Update results dictionary
       history["train_loss"].append(train_loss)
       history["train_acc"].append(train_acc)
       history["test_loss"].append(test_loss)
@@ -521,22 +322,10 @@ def train(model: torch.nn.Module,
       if test_acc > 0.95:
          break
 
-  # Return the filled results at the end of the epochs
   return model, history
-
-
-# In[27]:
-
 
 model, history = train(model, train_loader, test_loader, optimizer, loss_fn, num_epochs, device)
 
-
-# **viewing the learning history**
-
-# In[28]:
-
-
-#this function is a utility to help visualize and compare the performance of a model on training and validation datasets across epochs.
 def plot_history(history):
 
     loss = history['train_loss']
@@ -565,10 +354,6 @@ def plot_history(history):
     plt.title('Loss')
     plt.grid(True)
     plt.legend()
-
-
-# In[29]:
-
 
 plot_history(history)
 
